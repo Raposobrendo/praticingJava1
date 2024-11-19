1ab84a93626c461c982c93fb1cf656615d846c8c
Brendo Raposo <raposobrendo@gmail.com>
2024-11-19 17:01:09 -0300

    Beggining Date

---
+++ date/application/date1.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class date1 {
+	public static void main(String [] Args) {
+		
+		LocalDate d01 = LocalDate.now();
+		LocalDateTime d02 = LocalDateTime.now();
+		Instant d03 = Instant.now();
+		
+		System.out.println("d01: " + d01);
+		System.out.println("d02: " + d02);
+		System.out.println("d03: " + d03);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
